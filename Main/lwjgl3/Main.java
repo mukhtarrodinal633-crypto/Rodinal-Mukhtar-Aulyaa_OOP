99df155929d099008a240cc0ac58f6dafd50cb3e
Mukhtar <mukhtarrodinal633@gmail.com>
2026-09-02 00:04:09 +0700

    Update Main.java

--- Main/lwjgl3/Main.java
+++ Main/lwjgl3/Main.java
@@ -1,29 +1,28 @@
+package frontend;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== TOUHOU OOP PRACTICUM - MODULE 1: BASIC CLASSES & OBJECT INTERACTION ===");
 
-        Player player = new Player(
-            "Reimu Hakurei",
-            100,
-            15,
-            3
-        );
+        Player reimu = new Player("Reimu Hakurei", 100, 15, 3);
+        Enemy fairyBoss = new Enemy("Cirno (Stage 2 Boss)", 50);
 
-        Enemy enemy = new Enemy(
-            "Cirno",
-            50
-        );
+        System.out.println("\n--- Initial Battle State ---");
+        System.out.println("Player: " + reimu.name + " | HP: " + reimu.hp + " | Power: " + reimu.power + " | SpellCards: " + reimu.spellCards);
+        System.out.println("Enemy:  " + fairyBoss.name + " | HP: " + fairyBoss.hp);
 
-        System.out.println("=== PLAYER ===");
-        System.out.println("Name       : " + player.name);
-        System.out.println("HP         : " + player.hp);
-        System.out.println("Power      : " + player.power);
-        System.out.println("SpellCards : " + player.spellCards);
+        System.out.println("\n--- Turn 1: Player Shoots Enemy ---");
+        reimu.shoot(fairyBoss);
 
-        System.out.println();
+        System.out.println("\n--- Turn 2: Enemy Counter-attacks ---");
+        fairyBoss.attack(reimu, 30);
 
-        System.out.println("=== ENEMY ===");
-        System.out.println("Name       : " + enemy.name);
-        System.out.println("HP         : " + enemy.hp);
-        System.out.println("MaxHP      : " + enemy.maxHp);
+        System.out.println("\n--- Turn 3: Player Shoots Enemy Finishing Blow ---");
+        reimu.shoot(fairyBoss);
+
+        System.out.println("\n--- Turn 4: Enemy Deals Fatal Damage to Reimu ---");
+        fairyBoss.attack(reimu, 80);
+
+        System.out.println("\n=== Battle Simulation Complete ===");
     }
 }
